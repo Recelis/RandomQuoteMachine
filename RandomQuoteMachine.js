7c86e5ed848eaaa34a0e852065294a076dabfdba
JackyRecelis <jackymlui@live.com>
2017-02-15 22:43:53 +1000

    made HTML text change on button call and added API call to load text

--- RandomQuoteMachine.js
+++ RandomQuoteMachine.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
-    var url = "http://api.icndb.com/jokes/random?limitTo=[nerdy]"; 
+    var url = "http://api.icndb.com/jokes/random?limitTo=[nerdy]";
+     $.ajax({
+            type: "GET",
+            // dataType: "json",
+            url: url,
+            success: function(data){
+            console.log(data);
+            console.log(data["value"].joke);
+            $(".quote").html(data["value"].joke); // can affect text in html, here emphasis
+            // console.log(data["contents"].quotes[0].quote);
+            // http://www.icndb.com/api/
+        }
+        });
+    var quote; 
     $('#randQuote').click(function(){
         $.ajax({
             type: "GET",
@@ -8,11 +21,12 @@ $(document).ready(function() {
             success: function(data){
             console.log(data);
             console.log(data["value"].joke);
+             $(".quote").html(data["value"].joke); // can affect text in html, here emphasis
             // console.log(data["contents"].quotes[0].quote);
             // http://www.icndb.com/api/
         }
         });
-        alert("Testing 1 2 3... please remove");
+        // alert("Testing 1 2 3... please remove");
     });
 });
 
