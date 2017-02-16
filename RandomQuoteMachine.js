a67544eb68603bfbe452ef363b17b4959742c1d5
JackyRecelis <jackymlui@live.com>
2017-02-16 12:49:41 +1000

    all things working except twitter

--- RandomQuoteMachine.js
+++ RandomQuoteMachine.js
@@ -1,5 +1,19 @@
+var prevColor = 8;
+
+
 $(document).ready(function() {
     var url = "http://api.icndb.com/jokes/random?limitTo=[nerdy]";
+    var colorJSON = {
+        "red": "#FF0000",
+        "blue": "#0000FF",
+        "green": "#008000",
+        "yellow": "#bebe08",
+        "purple": "#800080",
+        "magenta": "#ff00ff",
+        "cyan": "#05c9c9",
+        "pink": "#d4909c",
+        "black": "#000000",
+    };
      $.ajax({
             type: "GET",
             // dataType: "json",
@@ -7,12 +21,12 @@ $(document).ready(function() {
             success: function(data){
             console.log(data);
             console.log(data["value"].joke);
-            $(".quote").html(data["value"].joke); // can affect text in html, here emphasis
+            $(".quote").html("\""+ data["value"].joke+"\""); // can affect text in html, here emphasis
             // console.log(data["contents"].quotes[0].quote);
             // http://www.icndb.com/api/
         }
         });
-    var quote; 
+    // var quote; 
     $('#randQuote').click(function(){
         $.ajax({
             type: "GET",
@@ -21,10 +35,22 @@ $(document).ready(function() {
             success: function(data){
             console.log(data);
             console.log(data["value"].joke);
-             $(".quote").html(data["value"].joke); // can affect text in html, here emphasis
-            // console.log(data["contents"].quotes[0].quote);
+            $("#box").hide().fadeIn(800);
+            $(".title").hide().fadeIn(800);
+            $(".quote").html("\""+ data["value"].joke+"\""); // can affect text in html, here emphasis
+            var size = Object.keys(colorJSON).length;
+            var randomColorIndex = prevColor;
+            while (randomColorIndex == prevColor){
+                randomColorIndex = Math.floor(Math.random() * size);
+                console.log("try again");
+            }
+            prevColor = randomColorIndex;
+            var colorKeysArr = Object.keys(colorJSON); // get colors in Array from colorJSON
+            $("#box").css("background-color", colorJSON[colorKeysArr[randomColorIndex]]);
+            $(".title").css("color", colorJSON[colorKeysArr[randomColorIndex]]);
+            console.log("the index is " + randomColorIndex + " color is " + colorKeysArr[randomColorIndex]);
             // http://www.icndb.com/api/
-        }
+            }
         });
         // alert("Testing 1 2 3... please remove");
     });
